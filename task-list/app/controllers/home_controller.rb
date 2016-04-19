1a25f2f3403c46230c0cad0f4ec64df7795f57e8
Anna Wilson <annie.marie.mason@gmail.com>
2016-04-19 15:58:33 -0700

    Task title link  renders a new view of that task

--- task-list/app/controllers/home_controller.rb
+++ task-list/app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+  def index
+    @all_tasks = Task.all
+  end
+
+  def single_task
+    @selected_task = Task.where(title: params[:title]).first
+    render :single_task
+  end
+
 end
