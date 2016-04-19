d07c8374ee7f9381ce8c46453a731a8d52830d0b
Jessica <jessica.weeber@gmail.com>
2016-04-19 14:09:57 -0700

    finished baseline. created tasks controller and model. migrated. added seeds.

---
+++ task-list/db/migrate/20160419201752_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+    	t.string :title, null: :false
+    	t.string :description, null: :false
+    	t.datetime :completed_on
+      t.timestamps null: false
+    end
+  end
+end
---
+++ task-list/db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Task.create!([
+{
+	title: "Get Passport",
+	description: "Get from post office; bring id"
+},
+
+{
+	title: "Create itinerary",
+	description: "Research itineraries from Lonely Planet book"
+},
+
+{
+	title: "Book rental car",
+	description: "Diesel, no AC needed. Cheaper."
+},
+
+{
+	title: "Book hotels",
+	description: "Booking.com to keep all together. At least 2 nights at one place"
+}
+	])
