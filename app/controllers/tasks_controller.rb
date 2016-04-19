9fbb8b86efee39338df2a0bcb3175ef35f8df988
Leah <leahnpetersen@gmail.com>
2016-04-19 16:44:47 -0700

    completed baseline, wave one, currently trying to hook up my delete button for wave 2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,15 @@
+class TasksController < ApplicationController
+  def index
+    #hooking up the db
+    # @albums = Album.order(artist: :asc)
+    @tasks = Task.order(id: :desc)
+  end
+
+  def show
+    @task = Task.where(id: params[:id]).first
+  end
+
+  def destroy
+    Task.where(id: params[:id]).destroy_all
+  end
+end
