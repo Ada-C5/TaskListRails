0282be23f8003b1d38788ec5379fc84fc6c42c38
Deirdre Storck <deirdre.storck@gmail.com>
2016-04-19 16:18:07 -0700

    got a touch of css, done with wave 1

--- taskmaster/app/controllers/tasks_controller.rb
+++ taskmaster/app/controllers/tasks_controller.rb
@@ -9,4 +9,21 @@ class TasksController < ApplicationController
     render :index
   end
 
+  def new
+    @new_task = Task.new
+
+  end
+
+  def create
+    Task.create(name: params[:task][:name], description: params[:task][:description])
+    redirect_to "/"
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+
 end
--- taskmaster/config/routes.rb
+++ taskmaster/config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "tasks#index"
+
+  get "/tasks/new" => "tasks#new"
   get "/:name" => "tasks#by_name"
+  post "/task" => "tasks#create"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
