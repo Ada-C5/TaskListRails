effd51939415717375dae863ebe462cb19a4e8c5
Rowan Cota <cota.rg@gmail.com>
2016-04-19 16:41:19 -0700

    working on primaries.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+
+	def index
+		@tasks = Task.order(id: :asc)
+		render :index
+	end
+
+	def show
+		@task = Task.where(id: params[:id])
+		render :detail_view
+	end
+
 end
