bd9c2eee45fb29261f89da06ca50051593d519de
sophia <sophiabaldonado@gmail.com>
2016-04-19 17:43:34 -0700

    Refactor how tasks are displayed, uses a partial for all tasks and renders based on completion status;

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,15 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task::all_tasks
+    @completed_tasks = @tasks.where(completed_at: nil)
+    @uncompleted_tasks = @tasks.where.not(completed_at: nil)
+
   end
 
   def show
     @tasks = Task::find_task(params[:id])
-    render :task
+    render :_task
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,16 @@
 class Task < ActiveRecord::Base
+
+  STATUS = {is_complete: ["uncomplete", "completed"], not_complete: ["completed", "uncompleted"]}
+
   def self.all_tasks
     Task.all
   end
 
   def self.find_task(id)
     [Task.find(id)]
+  end
+
+  def status
+     self.completed_at != nil ? STATUS[:is_complete] : STATUS[:not_complete]
   end
 end
