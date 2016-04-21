05322574283b7a82fa071e493821d955f0b0449c
Valerie <val.conklin@gmail.com>
2016-04-21 15:12:45 -0700

    Ahhhhh I forgot to use git for so long oh no. Hard to remember all that I did. I'm about to start working on the Edit button.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'tasks#index'
+
+  get '/tasks' => 'tasks#index'
+
+  get '/tasks/new' => 'tasks#new'
+
+  post '/tasks' => 'tasks#create'
+
+  get '/tasks/:id' => 'tasks#show', as: 'task'
+
+  delete '/tasks/:id' => 'tasks#destroy', as: 'delete'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
