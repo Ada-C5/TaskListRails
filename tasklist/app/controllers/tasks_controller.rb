2452310f66fee655cb6384ca54ada9c6e66e2d48
Lisa Rolczynski <lisa.sano@gmail.com>
2016-04-20 21:47:19 -0700

    added route to make a new task and a form to fill out

--- tasklist/app/controllers/tasks_controller.rb
+++ tasklist/app/controllers/tasks_controller.rb
@@ -6,4 +6,23 @@ class TasksController < ApplicationController
   def by_task
     @task = Task.where(id: params[:id]).first
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_create_params[:task])
+    if @task
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tasks_create_params
+    params.permit(task: [:title, :description, :completed_at])
+  end
 end
