80c9e9c87accd50693b1cdfe1500b10644ccd1de
Nadine Curry <nlcurry@gmail.com>
2016-04-21 12:56:55 -0700

    Added new task button and capability

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,30 @@ class HomeController < ApplicationController
   def index
     @tasks = Task.order(id: :asc)
   end
+
   def by_task
    @task = Task.where(id: params[:id])
    render :show
- end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_create_params[:task])
+    if (@task.save)
+      redirect_to task_path(@task.id)
+    else
+      render :new
+    end
+  end
+
+  def task_create_params
+    params.permit(task: [:name, :description])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get "/:id" => "home#by_task"
+  get"/tasks/new" => "home#new"
+  post "/tasks" => "home#create", as: "tasks"
+  get "/tasks/:id" => "home#show", as: "task"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
