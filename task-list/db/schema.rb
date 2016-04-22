e69c7e569dc8eac48eb44cbff2371e7a48e96735
Anna Wilson <annie.marie.mason@gmail.com>
2016-04-22 16:30:19 -0700

    Created Person model and seeded db

--- task-list/db/schema.rb
+++ task-list/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419203943) do
+ActiveRecord::Schema.define(version: 20160422231503) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title",        null: false
--- task-list/db/seeds.rb
+++ task-list/db/seeds.rb
@@ -1,20 +1,9 @@
-def random_time
-  Time.at(rand * Time.now.to_i)
-end
-
-tasks = [
-  { title: "The First Task", description: "", completed_at: random_time },
-  { title: "Go to Brunch", description: "" },
-  { title: "Go to Lunch", description: "", completed_at: random_time },
-  { title: "Go to Second Lunch", description: "" },
-  { title: "Play Video Games", description: "", completed_at: random_time },
-  { title: "High Five Somebody You Don't Know", description: "", completed_at: random_time },
-  { title: "Plant Flowers", description: "", completed_at: random_time },
-  { title: "Call Mom", description: "" },
-  { title: "She worries, you know.", description: "" },
-  { title: "Nap.", description: "", completed_at: random_time }
+people = [
+  { name: "Anna" },
+  { name: "Bryan" },
+  { name: "Mitch" }
 ]
 
-tasks.each do |task|
-  Task.create task
+people.each do |person|
+  Person.create person
 end
