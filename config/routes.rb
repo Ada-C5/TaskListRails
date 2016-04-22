782bf14d6f1414a00566b4d695f336115675354a
Leah <leahnpetersen@gmail.com>
2016-04-22 09:51:49 -0700

    added css and notes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
 
     root 'tasks#index'
     #get the form before you can fill it out  
-    get  '/tasks' => 'tasks#index', as: 'tasks' 
-    post '/tasks' => 'tasks#create'
-    get '/tasks/new' => 'tasks#new'
-    delete 'tasks/:id' => 'tasks#destroy'
-    get 'tasks/:id/' => 'tasks#show', as: 'task'
-    get 'tasks/:id/edit' => 'tasks#edit'
-    patch 'tasks/:id' => 'tasks#update', as: 'task_update'
+    #as: allows both get and post to use tasks route
+    get    '/tasks'         => 'tasks#index', as: 'tasks' 
+    post   '/tasks'         => 'tasks#create'
+    get    '/tasks/new'     => 'tasks#new'
+    delete 'tasks/:id'      => 'tasks#destroy'
+    get    'tasks/:id/'     => 'tasks#show', as: 'task'
+    get    'tasks/:id/edit' => 'tasks#edit'
+    patch  'tasks/:id'      => 'tasks#update', as: 'task_update'
+    #I can remove this path - somehow?
     patch 'tasks/:id/completed' => 'tasks#complete'
+
+    #examples
+    #get 'albums/by_year/:year' => 'albums#by_year'
+    #get 'artist/:id/albums' => 'artist#albums'
+    #resources :albums, only: [:index, :show]
 end
