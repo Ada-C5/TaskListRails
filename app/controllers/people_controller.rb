86e0978357e054b699f7eed3211a927119608e9c
Leah <leahnpetersen@gmail.com>
2016-04-22 15:56:50 -0700

    working on css

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,7 @@ class PeopleController < ApplicationController
   def show
     @person = Person.where(id: params[:id]).first
     @tasks = Task.where(person_id: @person.id)
-    # is there an active record where to search WHERE with an AND statement?
+    # is there an active record search WHERE with an AND statement?
     uncompleted = 0
     @tasks.each do |task|
       if task.completed_at == nil
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 def random_time
   Time.at(rand * Time.now.to_i)
 end
@@ -36,16 +30,3 @@ tasks.each do |task|
   person = Person.find(task[:person_id])
   person.tasks << Task.create(task)
 end
-
-
-# CSV.foreach(FILE_PATH, headers: true) do |row|
-#   row_hash = row.to_h
-#   row_hash.delete("artist")
-  
-#   # Album.create(row_hash)
-
-#   #create both tables and link them
-#   artist = Artist.find_or_create_by(name: artist_name)
-#   # .albums comes from has_many relationship
-#   artist.albums << Album.create(row_hash)
-# end
