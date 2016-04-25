1f2618d63ffe7b96ab835ae63a647dfa92a4ffa5
Rowan Cota <cota.rg@gmail.com>
2016-04-25 06:51:43 -0700

    updated so detail view showing all tasks belonging to a person works.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,12 +1,13 @@
 class PeopleController < ApplicationController
   def index
     @people = Person.order(name: :asc)
-    render :people
+    render :people_index
   end
 
   def show
     @person = Person.find(params[:id])
-    render :people
+    @tasks = Task.new
+    render :people_detail_view
   end
 
 
