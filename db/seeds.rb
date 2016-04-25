a6a64ea5b1496ff8bc93c8af6672f8aa572677d0
Jillian Boshart <Jilliliz@gmail.com>
2016-04-25 15:30:47 -0700

    I am learning by making many many many many many mistakes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,21 @@ def random_time
 end
 
 tasks = [
-  { task_name: "Wake up in the morning", description: "Feel like P-Diddy", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Grab my glasses", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Go out the door", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Hit this city", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Brush my teet", description: "With a bottle of Jack", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Leave for the night", description: "ain't coming back", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Pedicures", description: "on our toes toes", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Try on all our clothes", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Drop top", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Play favorite CD", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Roll up to the party", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "get a little bit tipsy", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Don't stop", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Make it pop", description: "", status: "", due_date: "", completed_on: random_time, peep_id: ""},
-  { task_name: "Tik Tok", description: "Important.", status: "", due_date: "", completed_on: random_time, peep_id: ""}
+  { task_name: "Wake up in the morning", description: "Feel like P-Diddy", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Grab my glasses", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Go out the door", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Hit this city", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Brush my teet", description: "With a bottle of Jack", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Leave for the night", description: "ain't coming back", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Pedicures", description: "on our toes toes", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Try on all our clothes", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Drop top", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Play favorite CD", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Roll up to the party", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "get a little bit tipsy", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Don't stop", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Make it pop", description: "", status: "", due_date: "", completed_on: "", peep_id: ""},
+  { task_name: "Tik Tok", description: "Important.", status: "", due_date: "", completed_on: "", peep_id: ""}
   ]
 
 tasks.each do |task|
