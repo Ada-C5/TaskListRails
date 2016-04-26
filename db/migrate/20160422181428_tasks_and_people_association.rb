469bd88523889a10d29151304e366eb79c6bc23e
Jessica <jessica.weeber@gmail.com>
2016-04-25 17:35:46 -0700

    reset db

--- db/migrate/20160422181428_tasks_and_people_association.rb
+++ db/migrate/20160422181428_tasks_and_people_association.rb
@@ -1,6 +1,5 @@
 class TasksAndPeopleAssociation < ActiveRecord::Migration
   def change
   	 add_column :tasks, :person_id, :integer
-  	 remove_column	:people, :task_id
   end
 end
--- db/migrate/20160426002623_change_task_id.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTaskId < ActiveRecord::Migration
-  def change
-  	remove_column :people, :task_id, :integer
-  end
-end
