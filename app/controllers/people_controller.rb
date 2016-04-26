fa6c8aea50ff89247ed392fd484c82fc46f04a20
Sarah <sarahkane682@yahoo.com>
2016-04-26 09:53:12 -0700

    more changes

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,7 +12,7 @@ class PeopleController < ApplicationController
   def all_tasklist
     @person = Person.find(params[:id])
     @list = @person.rails_task_lists.where(completion_status: nil)
-    @done_list = @person.rails_task_lists.where(completion_status: "done")
+    @done_list = @person.rails_task_lists.where(completion_status: "Done!")
   end
 
 end
